39c9407e273f2248d383e2b879665671f4f450bf
mowang7426 <972565726@qq.com>
2026-08-29 15:12:56 +0800

    Add files via upload

--- ControlCenter/SBCPUFloatingCCModuleViewController.m
+++ ControlCenter/SBCPUFloatingCCModuleViewController.m
@@ -2,12 +2,15 @@
 #import <Foundation/Foundation.h>
 #import <notify.h>
 
-// Control Center 模块单独使用 CFPreferences 读写，与 SBCPUFloating 的
-// LoadPreferences()/setBoolPref() 使用同一套偏好域，避免在 Control Center
-// 进程中调用 RootHide 路径扫描、文件迁移等复杂逻辑。
+// Control Center 进程只负责读写一个简单的 CFPreferences 开关。
+// 不调用 SpringBoard 对象、不访问 RootHide 路径、不创建悬浮窗。
 static CFStringRef kSBCPUFloatingCCPrefAppID = CFSTR("com.yourname.sbcpufloating");
 static CFStringRef kSBCPUFloatingCCPrefEnabledKey = CFSTR("isEnabled");
-static CFStringRef kSBCPUFloatingCCNotify = CFSTR("com.yourname.sbcpufloating.prefschanged");
+static const char *kSBCPUFloatingCCNotifyName = "com.yourname.sbcpufloating.prefschanged";
+
+static NSString *SBCPUCCString(const char *value) {
+    return value ? [NSString stringWithUTF8String:value] : [NSString stringWithUTF8String:"" ];
+}
 
 static BOOL SBCPUFloatingReadEnabled(void) {
     CFPropertyListRef value = CFPreferencesCopyValue(
@@ -23,8 +26,9 @@ static BOOL SBCPUFloatingReadEnabled(void) {
         enabled = CFBooleanGetValue((CFBooleanRef)value);
     } else if (CFGetTypeID(value) == CFNumberGetTypeID()) {
         int n = 0;
-        CFNumberGetValue((CFNumberRef)value, kCFNumberIntType, &n);
-        enabled = (n != 0);
+        if (CFNumberGetValue((CFNumberRef)value, kCFNumberIntType, &n)) {
+            enabled = (n != 0);
+        }
     }
     CFRelease(value);
     return enabled;
@@ -38,28 +42,32 @@ static void SBCPUFloatingWriteEnabled(BOOL enabled) {
         kCFPreferencesCurrentUser,
         kCFPreferencesAnyHost);
     CFPreferencesAppSynchronize(kSBCPUFloatingCCPrefAppID);
-    notify_post("com.yourname.sbcpufloating.prefschanged");
+    notify_post(kSBCPUFloatingCCNotifyName);
 }
 
 @interface SBCPUFloatingCCModuleViewController ()
-@property(nonatomic,strong) UIImageView *glyphOverlay;
-@property(nonatomic,assign) BOOL compactGlyphApplied;
+@property(nonatomic, strong) UIImageView *glyphOverlay;
+@property(nonatomic, assign) BOOL compactGlyphApplied;
+- (void)setupMenu;
 - (void)updateState;
-- (void)toggleFloating;
 @end
 
 @implementation SBCPUFloatingCCModuleViewController
 
+- (instancetype)init {
+    self = [super init];
+    return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.title = S("SBCPUFloating");
+    self.title = SBCPUCCString("SBCPUFloating");
 
     UIImageSymbolConfiguration *config = [UIImageSymbolConfiguration configurationWithPointSize:24.0 weight:UIImageSymbolWeightSemibold];
-    UIImage *glyph = [UIImage systemImageNamed:S("gauge.with.dots.needle.67percent") withConfiguration:config];
-    if (!glyph) glyph = [UIImage systemImageNamed:S("speedometer") withConfiguration:config];
-    if (!glyph) glyph = [UIImage systemImageNamed:S("cpu") withConfiguration:config];
+    UIImage *glyph = [UIImage systemImageNamed:SBCPUCCString("gauge.with.dots.needle.67percent") withConfiguration:config];
+    if (!glyph) glyph = [UIImage systemImageNamed:SBCPUCCString("speedometer") withConfiguration:config];
+    if (!glyph) glyph = [UIImage systemImageNamed:SBCPUCCString("cpu") withConfiguration:config];
     if ([self respondsToSelector:@selector(setGlyphImage:)]) [self setGlyphImage:glyph];
 
     if ([self respondsToSelector:@selector(setUseTallLayout:)]) [self setUseTallLayout:NO];
@@ -67,10 +75,11 @@ static void SBCPUFloatingWriteEnabled(BOOL enabled) {
     if ([self respondsToSelector:@selector(setUseTrailingCheckmarkLayout:)]) [self setUseTrailingCheckmarkLayout:YES];
     if ([self respondsToSelector:@selector(setShouldProvideOwnPlatter:)]) [self setShouldProvideOwnPlatter:NO];
 
-    _glyphOverlay = [UIImageView new];
-    _glyphOverlay.contentMode = UIViewContentModeScaleAspectFit;
-    _glyphOverlay.userInteractionEnabled = NO;
-    [self.view addSubview:_glyphOverlay];
+    self.glyphOverlay = [[UIImageView alloc] initWithFrame:CGRectZero];
+    self.glyphOverlay.contentMode = UIViewContentModeScaleAspectFit;
+    self.glyphOverlay.userInteractionEnabled = NO;
+    self.glyphOverlay.accessibilityElementsHidden = YES;
+    [self.view addSubview:self.glyphOverlay];
 
     [self setupMenu];
     [self updateState];
@@ -78,39 +87,39 @@ static void SBCPUFloatingWriteEnabled(BOOL enabled) {
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    [self refreshState];
+    [self updateState];
 }
 
 - (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
+
     CGRect bounds = self.view.bounds;
     BOOL compact = CGRectGetWidth(bounds) < 150.0 && CGRectGetHeight(bounds) < 150.0;
     self.glyphOverlay.hidden = !compact;
 
     if (compact) {
-        self.title = S("");
+        self.title = SBCPUCCString("");
         if (!self.compactGlyphApplied && [self respondsToSelector:@selector(setGlyphImage:)]) {
             [self setGlyphImage:[[UIImage alloc] init]];
             self.compactGlyphApplied = YES;
         }
+
         CGFloat icon = MIN(48.0, MIN(CGRectGetWidth(bounds) * 0.55, CGRectGetHeight(bounds) * 0.55));
-        self.glyphOverlay.frame = CGRectIntegral(CGRectMake((CGRectGetWidth(bounds)-icon)/2.0,
-                                                              (CGRectGetHeight(bounds)-icon)/2.0,
-                                                              icon, icon));
+        self.glyphOverlay.frame = CGRectIntegral(CGRectMake(
+            (CGRectGetWidth(bounds) - icon) / 2.0,
+            (CGRectGetHeight(bounds) - icon) / 2.0,
+            icon,
+            icon));
         [self.view bringSubviewToFront:self.glyphOverlay];
     } else if (self.compactGlyphApplied) {
         self.compactGlyphApplied = NO;
-        [self restoreHeaderGlyph];
+        UIImageSymbolConfiguration *config = [UIImageSymbolConfiguration configurationWithPointSize:24.0 weight:UIImageSymbolWeightSemibold];
+        UIImage *glyph = [UIImage systemImageNamed:SBCPUCCString("gauge.with.dots.needle.67percent") withConfiguration:config];
+        if (!glyph) glyph = [UIImage systemImageNamed:SBCPUCCString("speedometer") withConfiguration:config];
+        if (!glyph) glyph = [UIImage systemImageNamed:SBCPUCCString("cpu") withConfiguration:config];
+        if ([self respondsToSelector:@selector(setGlyphImage:)]) [self setGlyphImage:glyph];
         [self updateState];
     }
-}
-
-- (void)restoreHeaderGlyph {
-    UIImageSymbolConfiguration *config = [UIImageSymbolConfiguration configurationWithPointSize:24.0 weight:UIImageSymbolWeightSemibold];
-    UIImage *glyph = [UIImage systemImageNamed:S("gauge.with.dots.needle.67percent") withConfiguration:config];
-    if (!glyph) glyph = [UIImage systemImageNamed:S("speedometer") withConfiguration:config];
-    if (!glyph) glyph = [UIImage systemImageNamed:S("cpu") withConfiguration:config];
-    if ([self respondsToSelector:@selector(setGlyphImage:)]) [self setGlyphImage:glyph];
 }
 
 - (BOOL)shouldBeginTransitionToExpandedContentModule {
@@ -119,7 +128,7 @@ static void SBCPUFloatingWriteEnabled(BOOL enabled) {
 
 - (void)willTransitionToExpandedContentMode:(BOOL)animated {
     [super willTransitionToExpandedContentMode:animated];
-    [self refreshState];
+    [self updateState];
 }
 
 - (CGFloat)preferredExpandedContentHeight {
@@ -129,7 +138,7 @@ static void SBCPUFloatingWriteEnabled(BOOL enabled) {
 - (CGFloat)preferredExpandedContentWidth {
     CGFloat width = 300.0;
     CGFloat screenWidth = CGRectGetWidth([UIScreen mainScreen].bounds);
-    if (screenWidth > 0) width = MIN(width, screenWidth - 60.0);
+    if (screenWidth > 0.0) width = MIN(width, screenWidth - 60.0);
     return MAX(width, 220.0);
 }
 
@@ -144,14 +153,26 @@ static void SBCPUFloatingWriteEnabled(BOOL enabled) {
 - (void)setupMenu {
     __weak typeof(self) weakSelf = self;
     CCUIMenuModuleItem *item = [[CCUIMenuModuleItem alloc]
-        initWithTitle:S("SBCPUFloating")
-        identifier:S("sbcpufloating-toggle")
-        handler:^{ [weakSelf buttonTapped:nil forEvent:nil]; }];
+        initWithTitle:SBCPUCCString("SBCPUFloating")
+        identifier:SBCPUCCString("sbcpufloating-toggle")
+        handler:^{
+            SBCPUFloatingCCModuleViewController *strongSelf = weakSelf;
+            if (strongSelf) [strongSelf buttonTapped:nil forEvent:nil];
+        }];
+
+    if (!item) {
+        self.menuItems = @[];
+        return;
+    }
 
     if ([item respondsToSelector:@selector(setSubtitle:)]) {
-        [item setSubtitle:S("点击开启 / 关闭悬浮窗")];
+        [item setSubtitle:SBCPUCCString("点击开启 / 关闭悬浮窗")];
     }
-    self.menuItems = item ? @[item] : @[];
+    if ([item respondsToSelector:@selector(setSelected:)]) {
+        [item setSelected:SBCPUFloatingReadEnabled()];
+    }
+
+    self.menuItems = @[item];
     if ([self respondsToSelector:@selector(setMinimumMenuItems:)]) [self setMinimumMenuItems:1];
     if ([self respondsToSelector:@selector(setVisibleMenuItems:)]) [self setVisibleMenuItems:1];
 }
@@ -162,7 +183,11 @@ static void SBCPUFloatingWriteEnabled(BOOL enabled) {
 
 - (void)updateState {
     BOOL enabled = SBCPUFloatingReadEnabled();
-    self.title = enabled ? S("SBCPUFloating") : S("SBCPUFloating 已关闭");
+
+    BOOL compact = CGRectGetWidth(self.view.bounds) < 150.0 && CGRectGetHeight(self.view.bounds) < 150.0;
+    if (!compact) {
+        self.title = enabled ? SBCPUCCString("SBCPUFloating") : SBCPUCCString("SBCPUFloating 已关闭");
+    }
 
     if ([self respondsToSelector:@selector(setSelected:)]) {
         [self setSelected:enabled];
@@ -174,7 +199,7 @@ static void SBCPUFloatingWriteEnabled(BOOL enabled) {
     for (CCUIMenuModuleItem *item in self.menuItems) {
         if ([item respondsToSelector:@selector(setSelected:)]) [item setSelected:enabled];
         if ([item respondsToSelector:@selector(setSubtitle:)]) {
-            [item setSubtitle:enabled ? S("浮窗当前已开启") : S("浮窗当前已关闭")];
+            [item setSubtitle:enabled ? SBCPUCCString("浮窗当前已开启") : SBCPUCCString("浮窗当前已关闭")];
         }
         if ([item respondsToSelector:@selector(setSelectedGlyphColor:)]) {
             [item setSelectedGlyphColor:enabled ? [UIColor systemGreenColor] : [UIColor systemGrayColor]];
@@ -182,27 +207,24 @@ static void SBCPUFloatingWriteEnabled(BOOL enabled) {
     }
 
     UIImageSymbolConfiguration *configuration = [UIImageSymbolConfiguration configurationWithPointSize:42.0 weight:UIImageSymbolWeightSemibold];
-    UIImage *baseGlyph = [UIImage systemImageNamed:S("gauge.with.dots.needle.67percent") withConfiguration:configuration];
-    if (!baseGlyph) baseGlyph = [UIImage systemImageNamed:S("speedometer") withConfiguration:configuration];
-    if (!baseGlyph) baseGlyph = [UIImage systemImageNamed:S("cpu") withConfiguration:configuration];
-    UIImage *colored = [baseGlyph imageWithTintColor:(enabled ? [UIColor systemGreenColor] : [UIColor systemGrayColor])
-                                       renderingMode:UIImageRenderingModeAlwaysOriginal];
-    self.glyphOverlay.image = colored;
+    UIImage *baseGlyph = [UIImage systemImageNamed:SBCPUCCString("gauge.with.dots.needle.67percent") withConfiguration:configuration];
+    if (!baseGlyph) baseGlyph = [UIImage systemImageNamed:SBCPUCCString("speedometer") withConfiguration:configuration];
+    if (!baseGlyph) baseGlyph = [UIImage systemImageNamed:SBCPUCCString("cpu") withConfiguration:configuration];
+    if (baseGlyph) {
+        self.glyphOverlay.image = [baseGlyph imageWithTintColor:(enabled ? [UIColor systemGreenColor] : [UIColor systemGrayColor])
+                                                  renderingMode:UIImageRenderingModeAlwaysOriginal];
+    }
 }
 
 - (void)buttonTapped:(id)arg forEvent:(id)event {
     (void)arg;
     (void)event;
 
-    // 这是 Control Center 的原生模块点击入口。
-    // 只修改 isEnabled，不主动 dismiss、不创建窗口、不调用 RootHide 文件路径逻辑。
+    // 关键安全修复：点击 CC 时只做“读写一个布尔偏好 + Darwin 通知”。
+    // 不调用任何 SpringBoard 类、不触碰悬浮窗、不调用 120Hz/温控/快充代码。
     BOOL enabled = !SBCPUFloatingReadEnabled();
     SBCPUFloatingWriteEnabled(enabled);
     [self updateState];
-}
-
-- (void)toggleFloating {
-    [self buttonTapped:nil forEvent:nil];
 }
 
 @end
