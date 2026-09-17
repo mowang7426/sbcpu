c6c320ce2db39f159f0a389a2f265bdd82a29667
mowang7426 <972565726@qq.com>
2026-09-17 18:12:33 +0800

    Add files via upload

--- Shared/LGHostRegistry.h
+++ Shared/LGHostRegistry.h
@@ -47,7 +47,7 @@ typedef struct {
     X(TabBarSelection,"dylv.liquidglass.tabbarselect", "TabBarSelection",0.50f,         0.50f,                         18.0f, 2.2f, 1.75f, 0.0f, 1.0f,  0.0f, "#FFFFFF1A", "#FFFFFF14") \
     X(Keyboard,       "dylv.liquidglass.keyboard",     "Keyboard",      0.00f,          (28.0f / 220.0f) * 1.8f,       20.0f, 1.9f, 1.60f, 8.0f, 0.00f,  0.0f, "#FFFFFFCC", "#0000004d") \
     X(AppIcons,       "dylv.liquidglass.appicons",     "AppIcons",      28.0f / 220.0f, (28.0f / 220.0f) * 1.8f,       18.0f, 2.6f, 1.85f, 1.0f, 1.0f,  0.0f, "#FFFFFF1A", "#00000000") \
-    X(DynamicIsland,  "dylv.liquidglass.dynamicisland","DynamicIsland", 0.50f,          (28.0f / 220.0f) * 2.0f,       20.0f, 2.0f, 1.70f, 5.0f, 1.0f,  0.0f, "#FFFFFF1A", "#00000000")
+    X(DynamicIsland,  "dylv.liquidglass.dynamicisland","DynamicIsland", 0.50f,          (28.0f / 220.0f) * 2.0f,       20.0f, 2.0f, 1.70f, 5.0f, 1.0f,  0.0f, "#FFFFFF1A", "#00000000") \
     X(SBCPUFloating,  "dylv.liquidglass.sbcpufloating", "SBCPUFloating", 0.50f,          (28.0f / 220.0f) * 1.35f,      18.0f, 2.6f, 1.70f, 1.0f, 1.0f,  0.0f, "#FFFFFF12", "#00000000")
 
 enum LGHostIdentifier {
