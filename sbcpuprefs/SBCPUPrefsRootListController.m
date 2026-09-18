0cd01ed42ab6c8b6e545b977cc70cb36e138aeae
mowang7426 <972565726@qq.com>
2026-09-18 11:44:27 +0800

    Add files via upload

--- sbcpuprefs/SBCPUPrefsRootListController.m
+++ sbcpuprefs/SBCPUPrefsRootListController.m
@@ -1,3 +1,4 @@
+#import <UIKit/UIKit.h>
 
 #import "SBCPUPrefsRootListController.h"
 
@@ -8,6 +9,13 @@
 		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
 	}
 	return _specifiers;
+}
+
+- (void)openMoWangSource {
+	NSURL *url = [NSURL URLWithString:@"sileo://source/https://mowang7426.github.io/MoWang/"];
+	if ([[UIApplication sharedApplication] canOpenURL:url]) {
+		[[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+	}
 }
 
 @end
